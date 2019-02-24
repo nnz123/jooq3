8458f04d70646dcb2f4ab91edc1267eecf87dd51
zhangnannan <zhangnannan@ccbtrust.com.cn>
2019-02-24 11:27:20 +0800

    insert 方法还有 bad sql 问题没解决

---
+++ src/main/java/com/test/jooq3/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.test.jooq3.service.impl;
+
+
+import com.generator.tables.pojos.User;
+import com.generator.tables.records.UserRecord;
+import com.test.jooq3.service.UserService;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.groups.Default;
+import java.util.List;
+
+/**
+ * @author nzhang
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    DSLContext dslContext;
+    /**
+     * 为 com.generator.tables.User.USER重命名为u
+     */
+   private com.generator.tables.User u = com.generator.tables.User.USER.as("u");
+
+    @Override
+    public void delete(int id) {
+        dslContext.delete(u).where(u.ID.eq(id)).execute();
+    }
+
+    /**
+     * 这个测试还有问题
+     * @param user 用户
+     */
+    @Override
+    public void insert(User user) {
+        dslContext.insertInto(u).columns(u.USER_NAME,u.USER_AGE).values(user.getUserName(),user.getUserAge()).execute();
+
+    }
+
+    @Override
+    public void update(User user) {
+        dslContext.update(u).set(u.USER_NAME,user.getUserName()).set(u.USER_AGE,user.getUserAge()).where(u.ID.eq(user.getId())).execute();
+    }
+
+    @Override
+    public User getById(int id) {
+        List<User> list = dslContext.select().from(u).where(u.ID.eq(id)).fetchInto(User.class);
+        return list.get(0);
+    }
+
+    @Override
+    public List<User> get() {
+        return dslContext.select().from(u).fetchInto(User.class);
+    }
+}
